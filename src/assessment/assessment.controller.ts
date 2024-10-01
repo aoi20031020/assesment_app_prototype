b8024c3b54389db24a24a0a267162c0af8353787
aoi20031020 <aoi20031020@gmail.com>
2024-10-01 22:09:44 +0900

    protoの作成とドメインの実装コントローラ、サービスまで

---
+++ src/assessment/assessment.controller.ts
@@ -0,0 +1,30 @@
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { AssessmentService } from './assessment.service'; // Import AssesmentService
+import {
+  CreateAssessmentRequest,
+  CreateAssessmentResponse,
+  GetAssessmentByInsuranceNumberRequest,
+  GetAssessmentByInsuranceNumberResponse,
+} from 'src/protogen/assessment';
+
+@Controller()
+export class AssessmentController {
+  constructor(private readonly assessmentService: AssessmentService) {}
+
+  @GrpcMethod('AssessmentService', 'GetAssessmentByinsuranceNumber') // メソッド名を指定
+  async getAssessmentByinsuranceNumber(
+    data: GetAssessmentByInsuranceNumberRequest,
+  ): Promise<GetAssessmentByInsuranceNumberResponse> {
+    const assessment =
+      await this.assessmentService.getAssessmentByInsuranceNumber(data); // UserService の getUserById メソッドを呼び出し
+    return assessment; // UserService の getUserById メソッドが既に GetUserByIdResponse を返すと仮定
+  }
+
+  @GrpcMethod('AssessmentService', 'CreateAssessment')
+  async createAssessment(
+    data: CreateAssessmentRequest,
+  ): Promise<CreateAssessmentResponse> {
+    return this.assessmentService.createAssessment(data); // UserService の createUser メソッドを呼び出し
+  }
+}
---
+++ src/assessment/assessment.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import {
+  AssessmentServiceController,
+  CreateAssessmentRequest,
+  CreateAssessmentResponse,
+  GetAssessmentByInsuranceNumberRequest,
+  GetAssessmentByInsuranceNumberResponse,
+} from 'src/protogen/assessment';
+import { PrismaService } from '../../prisma/prisma.service';
+import { AssessmentRepository } from './assessment.repository'; // AssessmentRepository を適切にインポート
+import { mapAssessmentToProto } from './assessment-mapper';
+
+@Injectable()
+export class AssessmentService implements AssessmentServiceController {
+  constructor(
+    private readonly assessmentRepository: AssessmentRepository,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  // 被保険者番号で評価を取得
+  async getAssessmentByInsuranceNumber(
+    request: GetAssessmentByInsuranceNumberRequest,
+  ): Promise<GetAssessmentByInsuranceNumberResponse> {
+    const assessment = await this.assessmentRepository.findByInsuranceNumber(
+      request.insuranceNumber,
+    );
+    if (!assessment) {
+      throw new Error('User not found');
+    }
+    return mapAssessmentToProto(assessment); // マッピング関数を使用
+  }
+
+  // 新しい評価を作成
+  async createAssessment(
+    request: CreateAssessmentRequest,
+  ): Promise<CreateAssessmentResponse> {
+    try {
+      await this.assessmentRepository.createAssessment({
+        insuredPerson: request.insuredPerson,
+        emergencyContact: request.emergencyContact,
+        contact: request.contact,
+        familyStructure: request.familyStructure,
+        residenceInfo: request.residenceInfo,
+        toiletInfo: request.toiletInfo,
+        bathroomInfo: request.bathroomInfo,
+        mobilityInfo: request.mobilityInfo,
+      });
+
+      return { success: true }; // 成功時
+    } catch (error) {
+      console.error('Error creating assessment:', error);
+      return { success: false }; // エラー時
+    }
+  }
+}
