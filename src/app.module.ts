7c67c795d4b301d3a48e3f7a2cd4ac0190734c28
aoi20031020 <aoi20031020@gmail.com>
2024-10-04 00:48:54 +0900

    デモ機能のcrud作成したよ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module'; // ドメインモジュールをインポート
+import { DemoModule } from './demo/demo.module'; // ドメインモジュールをインポート
 
 @Module({
-  imports: [UserModule], // すべてのモジュールをここに追加
+  imports: [DemoModule], // すべてのモジュールをここに追加
 })
 export class AppModule {}
